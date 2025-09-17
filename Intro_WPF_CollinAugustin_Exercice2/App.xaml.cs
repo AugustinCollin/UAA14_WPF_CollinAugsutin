a5fa8d5c75b4dd8f97084321d0270282c1ebb236
Collin Augustin <240400@CCM-Asty-Moulin.local>
2025-09-17 09:59:18 +0200

    Prise en mains WPF

---
+++ Intro_WPF_CollinAugustin_Exercice2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Intro_WPF_CollinAugustin_Exercice2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
