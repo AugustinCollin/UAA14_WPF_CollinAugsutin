38afe33277ea8bc379cf0371b88ba47a92c0fc73
Collin Augustin <240400@CCM-Asty-Moulin.local>
2025-09-24 10:05:20 +0200

    Act1_Ex1_24/09/2025

---
+++ Act1_Exercice1_CollinAugustin/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Act1_Exercice1_CollinAugustin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
