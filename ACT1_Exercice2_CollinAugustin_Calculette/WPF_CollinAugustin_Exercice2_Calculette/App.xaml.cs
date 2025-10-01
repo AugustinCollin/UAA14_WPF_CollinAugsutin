dfeb390df919250bd8888463e20916412ae7f2ad
AugustinCollin <240400@site.asty-moulin.be>
2025-10-01 09:23:05 +0200

    exercice-calculette

---
+++ ACT1_Exercice2_CollinAugustin_Calculette/WPF_CollinAugustin_Exercice2_Calculette/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_CollinAugustin_Exercice2_Calculette
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
--- ACT1_Exercice2_CollinAugustin_Calculette/WPF_CollinAugustin_Exercice2_Calculette/obj/Debug/net8.0-windows/WPF_CollinAugustin_Exercice2_Calculette.AssemblyInfo.cs
+++ ACT1_Exercice2_CollinAugustin_Calculette/WPF_CollinAugustin_Exercice2_Calculette/obj/Debug/net8.0-windows/WPF_CollinAugustin_Exercice2_Calculette.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Act1_Exercice1_CollinAugustin")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WPF_CollinAugustin_Exercice2_Calculette")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38afe33277ea8bc379cf0371b88ba47a92c0fc73")]
-[assembly: System.Reflection.AssemblyProductAttribute("Act1_Exercice1_CollinAugustin")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Act1_Exercice1_CollinAugustin")]
+[assembly: System.Reflection.AssemblyProductAttribute("WPF_CollinAugustin_Exercice2_Calculette")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WPF_CollinAugustin_Exercice2_Calculette")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
